460db10b43b266be777c58343a9a51ca56e52ef4
pyj <jaremo@163.com>
2018-11-05 16:43:50 +0800

    生成二维码的工具类

---
+++ src/main/java/com/jaremo/freedom_talk/background/dao/CategoryDao.java
@@ -0,0 +1,9 @@
+package com.jaremo.freedom_talk.background.dao;
+
+/**
+ * @描述: 分类的数据访问层
+ * @Author: pyj
+ * @DateTime: 2018/11/5 0005--下午 3:44
+ */
+public interface CategoryDao {
+}
---
+++ src/main/java/com/jaremo/freedom_talk/background/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.jaremo.freedom_talk.background.service;
+
+/**
+ * @描述:
+ * @Author: pyj
+ * @DateTime: 2018/11/5 0005--下午 3:38
+ */
+public interface CategoryService {
+}
